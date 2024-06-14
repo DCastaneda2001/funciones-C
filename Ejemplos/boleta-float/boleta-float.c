2a45866a4e2848e5af92d2860a5d47836ed949b0
DCastaneda2001 <dcastaneda@ieee.org>
2024-06-13 23:35:33 -0600

    Subir archivos

---
+++ Ejemplos/boleta-float/boleta-float.c
@@ -0,0 +1,121 @@
+// Bibliotecas
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// Constantes
+#define NUM_MAT 6
+#define CAL_MIN 0
+#define CAL_APR 6
+#define CAL_MAX 10
+
+// Prototipos
+float lecturaDeCalificaciones(void);
+void materiasAprobadas(void);
+void materiasParaRecursar(void);
+
+// Variables globales
+float calificacion[NUM_MAT];
+int i;
+
+const char *materia[NUM_MAT] = {
+	"Calculo Diferencial e Integral",
+	"Fundamentos de Algebra",
+	"Fisica Clasica",
+	"Quimica Basica",
+	"Fundamentos de Programacion",
+	"Humanidades I"
+};
+	
+const char *grupo[NUM_MAT] = {
+	"1CV31",
+	"1CV32",
+	"1CV33",
+	"1CV34",
+	"1CV35",
+	"1CV36"
+};
+
+int main(void) {
+	
+	// Sentencias de ejecucion
+	printf("Promedio general: %.2f", lecturaDeCalificaciones());
+	materiasAprobadas();
+	materiasParaRecursar();
+	
+	// Cierre del programa
+	printf("\n\n\nPresione cualquier tecla para salir...");
+	getch();
+	
+	//Sentencia de retorno
+	return 0;
+	
+}
+
+float lecturaDeCalificaciones(void) {
+	
+	// Variables locales
+	float suma = 0;
+	
+	// Sentencias de ejecucion
+	for(i=0; i<NUM_MAT; i++) {
+		
+		// Solicitud y lectura de calificaciones
+		do {
+			
+			printf("\nIngrese la calificacion de %s (%s): ", materia[i], grupo[i]);
+			scanf("%f", &calificacion[i]);
+			
+			if(calificacion[i] < CAL_MIN || calificacion[i] > CAL_MAX) {
+				printf("\nCalificacion no valida.\n");
+			}
+			
+		} while(calificacion[i] < CAL_MIN || calificacion[i] > CAL_MAX);
+		
+		// Actualizacion de suma
+		suma = suma + calificacion[i];
+		
+	}
+	
+	// Limpiar consola
+	system("cls");
+	
+	// Sentencia de retorno
+	return suma / NUM_MAT;
+	
+	
+}
+
+void materiasAprobadas(void) {
+	
+	// Sentencias de ejecucion
+	printf("\n\n\n***Materias aprobadas***\n");
+	
+	for(i=0; i<NUM_MAT; i++) {
+		
+		if(calificacion[i] >= CAL_APR) {
+			
+			printf("\n   %s (%s)", materia[i], grupo[i]);
+
+		}
+
+	}
+	
+}
+
+void materiasParaRecursar(void) {
+
+	// Sentencias de ejecucion
+	printf("\n\n\n***Materias que debe recursar o presentar ETS***\n");
+
+	for(i=0; i<NUM_MAT; i++) {
+
+		if(calificacion[i] < CAL_APR) {
+			
+			printf("\n   %s (%s)", materia[i], grupo[i]);
+			
+		}
+		
+	}
+	
+}
